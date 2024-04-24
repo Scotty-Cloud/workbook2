01007287263f436836ae34d213727e9510f50c90
scott <scottysora@gmail.com>
2024-04-24 11:24:20 -0400

    edit carea of circle

--- exercises/circle/circleArea.js
+++ exercises/circle/circleArea.js
@@ -6,4 +6,4 @@ const areaOfCircle = () => {
 
 };
 
-console.log("Area of Circle is: " + areaOfCircle().toFixed(2));
+console.log("Area of Circle is: " + areaOfCircle().toFixed(2) + " with a radius of " + radius);
